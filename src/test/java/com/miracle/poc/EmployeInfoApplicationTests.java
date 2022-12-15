c78a0d9bf0fee7feef51921b703402fabf1cb4d7
chetan <bhurechetan766@gmail.com>
2022-12-15 14:03:56 -0500

    initial commit

---
+++ src/test/java/com/miracle/poc/EmployeInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracle.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
